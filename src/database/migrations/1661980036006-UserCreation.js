407e4795ef88144cfe60cda65528f9d132f0d460
Pablo Perales Molas <pperalesm@gmail.com>
2022-08-31 23:08:02 +0200

    Trying migrations

--- src/database/migrations/1661980036006-UserCreation.js
+++ src/database/migrations/1661980036006-UserCreation.js
@@ -1,10 +1,10 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner } = require('typeorm');
 
-module.exports = class UserCreation1661979498940 {
-    name = 'UserCreation1661979498940'
+module.exports = class UserCreation1661980036006 {
+  name = 'UserCreation1661980036006';
 
-    async up(queryRunner) {
-        await queryRunner.query(`
+  async up(queryRunner) {
+    await queryRunner.query(`
             CREATE TABLE "users" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "email" character varying NOT NULL,
@@ -26,11 +26,11 @@ module.exports = class UserCreation1661979498940 {
                 CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
             )
         `);
-    }
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`
+  async down(queryRunner) {
+    await queryRunner.query(`
             DROP TABLE "users"
         `);
-    }
-}
+  }
+};
