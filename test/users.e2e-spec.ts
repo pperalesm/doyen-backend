117440c1b8ded2e75f8f008f581ab7313dd7f3b2
Pablo Perales Molas <pperalesm@gmail.com>
2022-09-07 20:42:30 +0200

    Commented tests

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,160 +1,160 @@
-import request from 'supertest';
-import { Test, TestingModule } from '@nestjs/testing';
-import { HttpServer, INestApplication, ValidationPipe } from '@nestjs/common';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from '../src/users/users.module';
-import { Repository } from 'typeorm';
-import { User } from '../src/database/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
-import { MyTypeOrmLogger } from '../src/shared/util/my-typeorm-logger';
-import { HttpAdapterHost } from '@nestjs/core';
-import { AllExceptionsFilter } from '../src/shared/filters/all-exceptions.filter';
-import { SnakeNamingStrategy } from '../src/shared/util/snake-naming-strategy';
-import { AuthModule } from '../src/auth/auth.module';
+// import request from 'supertest';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { HttpServer, INestApplication, ValidationPipe } from '@nestjs/common';
+// import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+// import { UsersModule } from '../src/users/users.module';
+// import { Repository } from 'typeorm';
+// import { User } from '../src/database/entities/user.entity';
+// import { ConfigModule } from '@nestjs/config';
+// import { MyTypeOrmLogger } from '../src/shared/util/my-typeorm-logger';
+// import { HttpAdapterHost } from '@nestjs/core';
+// import { AllExceptionsFilter } from '../src/shared/filters/all-exceptions.filter';
+// import { SnakeNamingStrategy } from '../src/shared/util/snake-naming-strategy';
+// import { AuthModule } from '../src/auth/auth.module';
 
-describe('UsersController (e2e)', () => {
-  let app: INestApplication;
-  let server: HttpServer;
-  let usersRepository: Repository<User>;
-  let user1: User | null;
-  let user2: User | null;
+// describe('UsersController (e2e)', () => {
+//   let app: INestApplication;
+//   let server: HttpServer;
+//   let usersRepository: Repository<User>;
+//   let user1: User | null;
+//   let user2: User | null;
 
-  beforeAll(async () => {
-    const moduleRef: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-        }),
-        TypeOrmModule.forRoot({
-          type: 'postgres',
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-          database: 'test_users',
-          synchronize: true,
-          logging: ['warn'],
-          logger: new MyTypeOrmLogger(),
-          namingStrategy: new SnakeNamingStrategy(),
-          entities: [User],
-        }),
-        UsersModule,
-        AuthModule,
-      ],
-    }).compile();
+//   beforeAll(async () => {
+//     const moduleRef: TestingModule = await Test.createTestingModule({
+//       imports: [
+//         ConfigModule.forRoot({
+//           isGlobal: true,
+//         }),
+//         TypeOrmModule.forRoot({
+//           type: 'postgres',
+//           username: process.env.DB_USERNAME,
+//           password: process.env.DB_PASSWORD,
+//           database: 'test_users',
+//           synchronize: true,
+//           logging: ['warn'],
+//           logger: new MyTypeOrmLogger(),
+//           namingStrategy: new SnakeNamingStrategy(),
+//           entities: [User],
+//         }),
+//         UsersModule,
+//         AuthModule,
+//       ],
+//     }).compile();
 
-    app = moduleRef.createNestApplication();
-    server = app.getHttpServer();
-    usersRepository = moduleRef.get(getRepositoryToken(User));
-    app.useGlobalPipes(
-      new ValidationPipe({ whitelist: true, transform: true }),
-    );
-    const httpAdapter = app.get(HttpAdapterHost);
-    app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-    await app.init();
-  });
+//     app = moduleRef.createNestApplication();
+//     server = app.getHttpServer();
+//     usersRepository = moduleRef.get(getRepositoryToken(User));
+//     app.useGlobalPipes(
+//       new ValidationPipe({ whitelist: true, transform: true }),
+//     );
+//     const httpAdapter = app.get(HttpAdapterHost);
+//     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+//     await app.init();
+//   });
 
-  afterAll(async () => {
-    await usersRepository.query('DELETE FROM users');
-    await app.close();
-  });
+//   afterAll(async () => {
+//     await usersRepository.query('DELETE FROM users');
+//     await app.close();
+//   });
 
-  // it('/ (POST)', async () => {
-  //   let response = await request(server).post('/auth/signup').send({
-  //     email: 'hola@hola.com',
-  //     username: 'pepe',
-  //     password: 'password1243',
-  //   });
+// it('/ (POST)', async () => {
+//   let response = await request(server).post('/auth/signup').send({
+//     email: 'hola@hola.com',
+//     username: 'pepe',
+//     password: 'password1243',
+//   });
 
-  //   expect(response.status).toEqual(201);
-  //   expect(response.body).toStrictEqual({
-  //     id: response.body.id,
-  //     email: 'hola@hola.com',
-  //     username: 'pepe',
-  //     isPublic: false,
-  //     isVerified: false,
-  //     acceptsEmails: true,
-  //     language: 'en',
-  //     about: null,
-  //     avatarUrl: null,
-  //     name: null,
-  //     profession: null,
-  //   });
+//   expect(response.status).toEqual(201);
+//   expect(response.body).toStrictEqual({
+//     id: response.body.id,
+//     email: 'hola@hola.com',
+//     username: 'pepe',
+//     isPublic: false,
+//     isVerified: false,
+//     acceptsEmails: true,
+//     language: 'en',
+//     about: null,
+//     avatarUrl: null,
+//     name: null,
+//     profession: null,
+//   });
 
-  //   user1 = await usersRepository.findOne({
-  //     where: { id: response.body.id },
-  //   });
+//   user1 = await usersRepository.findOne({
+//     where: { id: response.body.id },
+//   });
 
-  //   expect(user1).toBeDefined();
-  //   expect({ ...user1 }).toStrictEqual({
-  //     id: user1?.id,
-  //     email: 'hola@hola.com',
-  //     username: 'pepe',
-  //     password: user1?.password,
-  //     name: null,
-  //     avatarUrl: null,
-  //     about: null,
-  //     profession: null,
-  //     gains: 0,
-  //     isPublic: false,
-  //     isVerified: false,
-  //     acceptsEmails: true,
-  //     language: 'en',
-  //     bannedUntil: null,
-  //     createdAt: user1?.createdAt,
-  //   });
+//   expect(user1).toBeDefined();
+//   expect({ ...user1 }).toStrictEqual({
+//     id: user1?.id,
+//     email: 'hola@hola.com',
+//     username: 'pepe',
+//     password: user1?.password,
+//     name: null,
+//     avatarUrl: null,
+//     about: null,
+//     profession: null,
+//     gains: 0,
+//     isPublic: false,
+//     isVerified: false,
+//     acceptsEmails: true,
+//     language: 'en',
+//     bannedUntil: null,
+//     createdAt: user1?.createdAt,
+//   });
 
-  //   response = await request(server).post('/auth/signup').send({
-  //     email: 'adios@adios.com',
-  //     username: 'pepe2',
-  //     password: 'afasfaasdasd',
-  //     name: 'asfasf',
-  //     avatarUrl: 'as',
-  //     about: 'as',
-  //     profession: 'null',
-  //     isPublic: true,
-  //     acceptsEmails: false,
-  //     language: 'en',
-  //   });
+//   response = await request(server).post('/auth/signup').send({
+//     email: 'adios@adios.com',
+//     username: 'pepe2',
+//     password: 'afasfaasdasd',
+//     name: 'asfasf',
+//     avatarUrl: 'as',
+//     about: 'as',
+//     profession: 'null',
+//     isPublic: true,
+//     acceptsEmails: false,
+//     language: 'en',
+//   });
 
-  //   expect(response.status).toEqual(201);
-  //   expect(response.body).toStrictEqual({
-  //     id: response.body.id,
-  //     email: 'adios@adios.com',
-  //     username: 'pepe2',
-  //     isPublic: true,
-  //     isVerified: false,
-  //     acceptsEmails: false,
-  //     language: 'en',
-  //     about: 'as',
-  //     avatarUrl: 'as',
-  //     name: 'asfasf',
-  //     profession: 'null',
-  //   });
+//   expect(response.status).toEqual(201);
+//   expect(response.body).toStrictEqual({
+//     id: response.body.id,
+//     email: 'adios@adios.com',
+//     username: 'pepe2',
+//     isPublic: true,
+//     isVerified: false,
+//     acceptsEmails: false,
+//     language: 'en',
+//     about: 'as',
+//     avatarUrl: 'as',
+//     name: 'asfasf',
+//     profession: 'null',
+//   });
 
-  //   user2 = await usersRepository.findOne({
-  //     where: { id: response.body.id },
-  //   });
+//   user2 = await usersRepository.findOne({
+//     where: { id: response.body.id },
+//   });
 
-  //   expect(user2).toBeDefined();
-  //   expect({ ...user2 }).toStrictEqual({
-  //     id: user2?.id,
-  //     email: 'adios@adios.com',
-  //     username: 'pepe2',
-  //     password: user2?.password,
-  //     name: 'asfasf',
-  //     avatarUrl: 'as',
-  //     about: 'as',
-  //     profession: 'null',
-  //     isPublic: true,
-  //     acceptsEmails: false,
-  //     language: 'en',
-  //     bannedUntil: null,
-  //     gains: 0,
-  //     isVerified: false,
-  //     createdAt: user2?.createdAt,
-  //   });
-  // });
+//   expect(user2).toBeDefined();
+//   expect({ ...user2 }).toStrictEqual({
+//     id: user2?.id,
+//     email: 'adios@adios.com',
+//     username: 'pepe2',
+//     password: user2?.password,
+//     name: 'asfasf',
+//     avatarUrl: 'as',
+//     about: 'as',
+//     profession: 'null',
+//     isPublic: true,
+//     acceptsEmails: false,
+//     language: 'en',
+//     bannedUntil: null,
+//     gains: 0,
+//     isVerified: false,
+//     createdAt: user2?.createdAt,
+//   });
+// });
 
-  it('/ (GET)', () => {
-    expect('Test').toBeDefined();
-  });
-});
+//   it('/ (GET)', () => {
+//     expect('Test').toBeDefined();
+//   });
+// });
